4512dd5bce8fd93300e8cb954fb81bf7a055cd11
zenitsu <afcompshd@gmail.com>
2023-08-30 11:29:24 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+app.use(cors())
+
+app.use('/', (req, res) => {
+    res.send('Hello World')
+})
+
+const port = process.env.PORT || 5000
+app.listen(port, console.log('server is running'))
